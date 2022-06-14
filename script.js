af1b911f79b86a641d9e5b62b0d843817c4643c6
FlavioLSS <flavio.lucas.ss13@gmail.com>
2022-06-14 17:47:01 -0300

    commit js

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 
-let h2 = document.querySelector('h2');
+const h2 = document.querySelector('h2');
 
 function success(position){
     console.log(position.coords.latitude, position.coords.longitude);
     h2.textContent = `Latitude: ${position.coords.latitude} Longitude: ${position.coords.longitude}`;
 }
 
-navigator.geolocation.getCurrentPosition(success);
+function error(err) {
+    console.log(err);
+    h2.textContent = `Error: ${'Você negou o acesso a localização'}`;
+}
+
+var watchId = navigator.geolocation.watchPosition(success, error, {
+    enableHighAccuracy: true,
+    timeout: 5000
+});
+
+// navigator.geolocation.clearWatch(watchId);
