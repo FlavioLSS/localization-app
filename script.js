91a1192032ab56278ce4e4a19460dd8caf403bfd
FlavioLSS <flavio.lucas.ss13@gmail.com>
2022-06-14 18:24:39 -0300

    mapa

--- script.js
+++ script.js
@@ -4,6 +4,15 @@ const h2 = document.querySelector('h2');
 function success(position){
     console.log(position.coords.latitude, position.coords.longitude);
     h2.textContent = `Latitude: ${position.coords.latitude} Longitude: ${position.coords.longitude}`;
+    var map = L.map('map').setView([position.coords.latitude, position.coords.longitude], 13);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    L.marker([position.coords.latitude, position.coords.longitude]).addTo(map)
+        .bindPopup('Você está aqui.')
+        .openPopup();
 }
 
 function error(err) {
